c6b9c47af77ee8cbaea172ef94c50265726074dd
DomonkosSike <siked@kkszki.hu>
2024-11-25 10:13:14 +0100

    Kész a ComputerController, HTTPGET(id),HTTPOST

--- ComputerApi/Controllers/computerController.cs
+++ ComputerApi/Controllers/computerController.cs
@@ -1,11 +1,56 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ComputerApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComputerApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("comp")]
     [ApiController]
     public class computerController : ControllerBase
     {
+        private readonly ComputerContext computerContext;
+
+        public computerController(ComputerContext computerContext)
+        {
+            this.computerContext = computerContext;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Comp>> Post(CreateComputerDto createComputerDto)
+        {
+            var cmp = new Comp
+            {
+                Id = Guid.NewGuid(),
+                Brand = createComputerDto.brand,
+                Type = createComputerDto.type,
+                Display = createComputerDto.display,
+                Memory = createComputerDto.memory,
+                CreatedTime = DateTime.Now,
+                Osld = createComputerDto.Osld
+            };
+
+            if (cmp != null)
+            {
+                await computerContext.Comps.AddAsync(cmp);
+                await computerContext.SaveChangesAsync();
+                return StatusCode(201, cmp);
+            }
+            return BadRequest();
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetAllCompByID(Guid id)
+        {
+            var os = computerContext.Osystems.Include(os => os.Comps).Where(c => c.Id == id);
+
+            if (os != null)
+            {
+                return Ok(os);
+            }
+            return BadRequest();
+        }
+
+        
     }
+
 }
--- ComputerApi/Models/Dto.cs
+++ ComputerApi/Models/Dto.cs
@@ -3,4 +3,6 @@
     public record CreatedOsDto(string? Name);
     
     public record UpdateOsDto(string? Name);
+
+    public record CreateComputerDto(Guid? id,string brand,string type,double display,int memory,DateTime CreatedTime,Guid Osld);
 }
